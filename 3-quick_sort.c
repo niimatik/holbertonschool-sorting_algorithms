a0e8cbcfadedae1c564bcb637fac77ea4ff44eef
niimatik <niimatik.gg@gmail.com>
2025-08-06 10:27:10 +0200

    docs: en cours

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,47 @@
 #include "sort.h"
 
+void swap(int *src, int *dest)
+{
+	int tmp;
+
+	tmp = *src;
+	*src = *dest;
+	*dest = tmp;
+}
+
+/**
+ * lomuto_sort - sorts an array of integers in ascending order
+ * using the Quick sort algorithm
+ * @array: the array to sort
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int *pivot, above, bellow;
+
+	pivot = array + right;
+	for (above = bellow = left; bellow < right; bellow++)
+	{
+		if (array[bellow] < *pivot)
+		{
+			if (above < bellow)
+			{
+				swap(array + bellow, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
+		if (array[above] > *pivot)
+		{
+			swap(array + above, pivot);
+			print_array(array, size);
+		}
+	}
+}
+
 /**
  * quick_sort - sorts an array of integers in ascending order
  * using the Quick sort algorithm
@@ -14,41 +56,5 @@ void quick_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	lomuto_sort(array, size)
-}
-
-/**
- * lomuto_sort - sorts an array of integers in ascending order
- * using the Quick sort algorithm
- * @array: the array to sort
- * @size: the size of the array
- *
- * Return: void
- */
-
-void lomuto_sort(int *array, size_t size)
-{
-	size_t i = 0, j = size - 1, tmp, pivot;
-
-	while (i < size - 1)
-	{
-		tmp = i;
-		while (j > i)
-		{
-			if (i > j)
-			{
-				swap(array[i], array[j]);
-			}
-
-		}
-	}
-}
-
-void swap(size_t src, size_t dest)
-{
-	size_t tmp;
-
-	tmp = src;
-	src = dest;
-	dest = tmp;
+	lomuto_sort(array, size, 0, size - 1);
 }
